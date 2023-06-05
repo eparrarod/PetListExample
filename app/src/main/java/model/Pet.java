c5d96f43d1327f5f432306e179b60d2a8c8c8000
estebanparrarodriguez <esteban.parrarodriguez@belmont.edu>
2023-06-05 08:55:18 -0500

    Added member data to Pet class and a constructor

--- app/src/main/java/model/Pet.java
+++ app/src/main/java/model/Pet.java
@@ -2,5 +2,15 @@ package model;
 
 public class Pet {
 
+    private String name;
+    private String breed;
+    private int age;
+
+    public Pet(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
 
 }
