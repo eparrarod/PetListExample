e58e3e51062cc5d856666b87e42be66d397fad13
estebanparrarodriguez <esteban.parrarodriguez@belmont.edu>
2023-06-05 09:31:03 -0500

    Added adapter to populate list with pets list

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.listviewexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.LinkedList;
+
+import model.Pet;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int selection = 1;
+        listView = findViewById(R.id.list);
+        int selection = 2;
         populateListView(selection);
     }
 
@@ -33,12 +41,25 @@ public class MainActivity extends AppCompatActivity {
      */
     private void useStringResource() {
         String[] breeds = getResources().getStringArray(R.array.breeds);
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, breeds);
+        listView.setAdapter(adapter);
     }
 
     /**
      * This method populates the list view using a string resource containing the list of a data array
      */
     private void usingDataArray() {
+
+        LinkedList<Pet> pets = new LinkedList<>();
+        pets.add(new Pet("Kenny", "Husky", 14));
+        pets.add(new Pet("Roger", "Golder Retriever", 13));
+        pets.add(new Pet("Bond", "Samoyed", 4));
+        pets.add(new Pet("Mango", "Whimpet", 2));
+        pets.add(new Pet("Boy", "Mutt", 6));
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pets);
+
+        listView.setAdapter(adapter);
 
     }
 }
